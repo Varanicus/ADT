b5420c973017bb160eb4987907508bc05ae84ac4
FlorianBoldt <FlorianBoldt@0-209-12>
2022-09-23 08:34:25 +0200

    tree

--- ADT/src/binarytree/BuildTree.java
+++ ADT/src/binarytree/BuildTree.java
@@ -3,11 +3,35 @@ package binarytree;
 public class BuildTree
 {
    public Element root;
+   public 	Element aknoten=root;
 
    public BuildTree()
    {
       root = new Element(100);
       root.left = new Element(50);
       root.right = new Element(150);   
+   }
+   
+   
+   
+   public void insert(int value) {
+	   
+
+	   if(aknoten.value > value) {
+		   if(root.left ==null) {
+			   root.left= new Element(value);
+		   }else {
+			   aknoten=root.left;
+			   insert(value);
+		   }
+		   
+	   }else {
+		   if(root.right ==null) {
+			   root.right= new Element(value);
+		   }else {
+			   aknoten=root.right;
+			   insert(value);
+		   }
+	   }
    }
 }
